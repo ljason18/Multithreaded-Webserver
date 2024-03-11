4760b42c2e0d78f1c7644e3f3da7520189a85953
ljason18 <jl71549@uga.edu>
2024-03-11 19:52:33 -0400

    initial commit

---
+++ WebServer.java
@@ -0,0 +1,152 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public final class WebServer {
+    public static void main(String argv[]) throws Exception {
+        // Set the port number
+        int port = 6789;
+        // Establish the listen socket.
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        // Process HTTP service requests in an infinite loop.
+        while (true) {
+            // Listen for a TCP connection request.
+            Socket clientSocket = serverSocket.accept();
+
+            // Construct an object to process the HTTP request message.
+            HttpRequest request = new HttpRequest(clientSocket);
+
+            // Create a new thread to process the request.
+            Thread thread = new Thread(request);
+
+            // Start the thread.
+            thread.start();
+        }
+    }
+}
+
+final class HttpRequest implements Runnable {
+
+    final static String CRLF = "\r\n";
+    Socket socket;
+
+    // Constructor
+    public HttpRequest(Socket clientSocket) throws Exception {
+        this.socket = clientSocket;
+    }
+
+    // Implement the run() method of the Runnable interface.
+    public void run() {
+        try {
+            processRequest();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private void processRequest() throws Exception {
+        // Get a reference to the socket's input and output streams.
+        InputStream is = socket.getInputStream();
+        OutputStream o = socket.getOutputStream();
+        DataOutputStream os = new DataOutputStream(o);
+
+        // Set up input stream filters.
+        InputStreamReader ir = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(ir);
+
+        // Get the request line of the HTTP request message
+        String requestLine = br.readLine();
+
+        // Display the request line
+        System.out.println();
+        System.out.println(requestLine);
+
+        // Extract the filename from the request line
+        StringTokenizer tokens = new StringTokenizer(requestLine);
+        tokens.nextToken(); // Skip over the method, which should be "GET"
+        String fileName = tokens.nextToken();
+
+        // Prepend a "." so that the file request is within the current direcotory
+        fileName = "." + fileName;
+
+        // Open the requested file
+        FileInputStream fis = null;
+        boolean fileExists = true;
+        try {
+            fis = new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            fileExists = false;
+        }
+
+        // Construct the respose message
+        String statusLine = null;
+        String contentTypeLine = null;
+        String entityBody = null;
+        if (fileExists) {
+            statusLine = "HTTP/1.1 200 OK" + CRLF;
+            contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+        } else {
+            statusLine = "HTTP/1.1 404 Not Found";
+            contentTypeLine = "Content-type: " + contentType(fileName) + CRLF;
+            entityBody = "<HTML>" + "<HEAD><TITLE>Not Found</TITLE></HEAD>" + "<BODY>Not Found</BODY></HTML>";
+        }
+        try {
+            // Send the status line
+            os.writeBytes(statusLine);
+            
+            // Send the content type line
+            os.writeBytes(contentTypeLine);
+            
+            // Get and display the header linies
+            String headerLine = null;
+            while ((headerLine = br.readLine()).length() != 0) {
+                System.out.println(headerLine);
+            }
+            
+            // Send a blank line to indicate the end of the header lines
+            os.writeBytes(CRLF);
+            
+            // Send the entity body.
+            if (fileExists) {
+                sendBytes(fis, os);
+                fis.close();
+            } else {
+                os.writeBytes(entityBody);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+         
+        // Close streams and socket
+        os.close();
+        br.close();
+        socket.close();
+    }
+
+    private static void sendBytes(FileInputStream fis, OutputStream os) throws Exception {
+        // Construct a 1K buffer to hold bytes on their way to the socket.
+        byte[] buffer = new byte[1024];
+        int bytes = 0;
+        // Copy requested file into the socket's output stream.
+        while ((bytes = fis.read(buffer)) != -1) {
+            os.write(buffer, 0, bytes);
+        }
+    }
+
+    private static String contentType(String fileName) {
+        if (fileName.endsWith(".htm") || fileName.endsWith(".html")) {
+            return "text/html";
+        }
+        if(fileName.endsWith(".gif")) {
+            return "image/gif";
+        }
+        if(fileName.endsWith(".jpeg")) {
+            return "image/jpeg";
+        }
+        if(fileName.endsWith(".png")) {
+            return "image/png";
+        }
+        return "application/octet-stream";
+    }
+}
